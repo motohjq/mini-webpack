230ee95a9dd92ea4fd2592ac3b67f43b3adf89c5
huangjiaqing <huangjiaqing@bilibili.com>
2024-05-28 22:18:53 +0800

    实现mini-webpack

---
+++ dist/0.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./src/title.js":
+/*!**********************!*\
+  !*** ./src/title.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = 'title';\n\n//# sourceURL=webpack:///./src/title.js?");
+
+/***/ })
+
+}]);
